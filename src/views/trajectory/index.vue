ecbdc1cdb43b4bb23cc3b96610739f80ec17b8d9
wangxs <15037589151@163.com>
2020-05-30 17:52:05 +0800

    12

--- src/views/trajectory/index.vue
+++ src/views/trajectory/index.vue
@@ -332,6 +332,9 @@ export default {
           offset : new BMap.Size(-13,-15)    //设置文本偏移量
         }
         let marker = new BMap.Marker(point, opts);  // 创建文本标注对象
+        let conten=`<div style="width:80px;">${iteam.fName}</div>`
+        var label = new BMap.Label(conten,{offset:new BMap.Size(-20,-28)});
+        marker.setLabel(label);
         marker.addEventListener("click",()=>{
           this.showIcl(iteam)
           this.showmillInform(iteam)
